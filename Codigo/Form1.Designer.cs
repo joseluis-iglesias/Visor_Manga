73f0f34052fcf794f51dab84ef2127bb931a5295
joseluis-iglesias <joseiglesiasfernandez2@gmail.com>
2018-01-02 19:27:37 +0100

    Código
    Subida del código  base del proyecto

---
+++ Codigo/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace Manga
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 37);
+            this.button1.Name = "Cargar Ultimos Manga";
+            this.button1.Size = new System.Drawing.Size(168, 44);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Cargar Ultimos Mangas";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 238);
+            this.button2.Name = "Mostrar Manga";
+            this.button2.Size = new System.Drawing.Size(168, 55);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Mostrar Manga";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(1851, 822);
+            this.button3.Name = "Atras";
+            this.button3.Size = new System.Drawing.Size(93, 32);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Atras";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(1950, 822);
+            this.button4.Name = "Siguiente";
+            this.button4.Size = new System.Drawing.Size(93, 32);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Siguiente";
+            this.button4.UseVisualStyleBackColor = true;
+            this.comboBox1.TabIndex = 2;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Location = new System.Drawing.Point(395, 37);
+            this.groupBox1.Name = "Manga";
+            this.groupBox1.Size = new System.Drawing.Size(1271, 794);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Manga";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 107);
+            this.comboBox1.Name = "Mangas";
+            this.comboBox1.Size = new System.Drawing.Size(248, 24);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(12, 174);
+            this.comboBox2.Name = "Capitulos";
+            this.comboBox2.Size = new System.Drawing.Size(248, 24);
+            this.comboBox2.TabIndex = 6;
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(1795, 57);
+            this.comboBox3.Name = "Paginas";
+            this.comboBox3.Size = new System.Drawing.Size(248, 24);
+            this.comboBox3.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(2171, 866);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Manga";
+            this.Text = "Manga";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox3;
+    }
+}
+
---
+++ Codigo/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Manga
+{
+    public partial class Form1 : Form
+    {
+        WebBrowser Navegador = new WebBrowser();
+        public Form1()
+        {
+            InitializeComponent();
+            Navegador.DocumentCompleted += new WebBrowserDocumentCompletedEventHandler(Navegador_DocumentCompleted);
+        }
+
+        void Navegador_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+
+        {
+
+            var Cadena = Navegador.Document.ToString();
+
+            EscribirArchivoTexto("texto1.txt", Cadena);
+
+        }
+
+
+
+        private void EscribirArchivoTexto(string NombreArchivo, string Contenido)
+
+        {
+
+            FileStream stream = new FileStream(NombreArchivo, FileMode.OpenOrCreate, FileAccess.Write);
+
+            StreamWriter writer = new StreamWriter(stream);
+
+            writer.WriteLine(Contenido);
+
+            writer.Close();
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Navegador.Navigate("https://www.tumangaonline.com/lector/Shokugeki-no-Soma/8373/244.00/7634");
+        }
+    }
+}
