41a98358c03a99210a1e612b56fcba52c17c2b50
joseluis-iglesias <joseiglesiasfernandez2@gmail.com>
2018-01-02 22:19:34 +0100

    Mejora de funcionamiento

--- Codigo/Form1.cs
+++ Codigo/Form1.cs
@@ -8,43 +8,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Manga;
 
 namespace Manga
 {
     public partial class Form1 : Form
     {
-        WebBrowser Navegador = new WebBrowser();
+        
         public Form1()
         {
             InitializeComponent();
-            Navegador.DocumentCompleted += new WebBrowserDocumentCompletedEventHandler(Navegador_DocumentCompleted);
         }
 
-        void Navegador_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
-
-        {
-
-            var Cadena = Navegador.Document.ToString();
-
-            EscribirArchivoTexto("texto1.txt", Cadena);
-
-        }
-
-
-
-        private void EscribirArchivoTexto(string NombreArchivo, string Contenido)
-
-        {
-
-            FileStream stream = new FileStream(NombreArchivo, FileMode.OpenOrCreate, FileAccess.Write);
-
-            StreamWriter writer = new StreamWriter(stream);
-
-            writer.WriteLine(Contenido);
-
-            writer.Close();
-
-        }
+        
 
         private void groupBox1_Enter(object sender, EventArgs e)
         {
@@ -53,7 +29,15 @@ namespace Manga
 
         private void button1_Click(object sender, EventArgs e)
         {
-            Navegador.Navigate("https://www.tumangaonline.com/lector/Shokugeki-no-Soma/8373/244.00/7634");
+            try
+            {
+                Manga.CargarPagina();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
         }
+       
     }
 }
---
+++ Codigo/Manga.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Manga
+{
+    public class Manga
+    {
+        static WebBrowser Navegador = new WebBrowser();
+        static String Ruta = "Home";
+        static String navegar = "https://www.tumangaonline.com/";
+
+        public static void Main() { }
+
+        /// <summary>
+        /// Punto de entrada principal para la aplicación.
+        /// </summary>
+        public static void CargarPagina(String nuevaRuta = null)
+        {
+            if (nuevaRuta == null)
+                Navegador.Navigate(navegar + Ruta);
+            else
+                Navegador.Navigate(navegar + nuevaRuta);
+        }
+
+        void Navegador_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            try
+            {
+                var Cadena = Navegador.DocumentText.ToString();
+                Cadena = ExtraerLogin(Cadena);
+                CargarPagina(Cadena);
+                Cadena = Navegador.DocumentText.ToString();
+                EscribirArchivoTexto("texto.txt", Cadena);
+                Navegador.Dispose();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        private static String ExtraerLogin(string Contenido)
+        {
+            String subStrMan = Contenido.Substring(Contenido.IndexOf("<ng-include ng-hide"), Contenido.IndexOf("<ng-include ng-show=\"visor\"") - Contenido.IndexOf("<ng-include ng-hide"));
+            return subStrMan;
+        }
+
+        private static void EscribirArchivoTexto(string NombreArchivo, string Contenido)
+
+        {
+
+            FileStream stream = new FileStream(NombreArchivo, FileMode.OpenOrCreate, FileAccess.Write);
+
+            StreamWriter writer = new StreamWriter(stream);
+
+            writer.WriteLine(Contenido);
+
+            writer.Close();
+
+        }
+    }
+}
